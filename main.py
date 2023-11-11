96420bdb03cdce00cc9a2327b6fbe59a6fd7949d
Gabriel Antônio <gabriel.antonio.dlm@gmail.com>
2023-11-11 14:06:21 -0300

    Update main.py
    Otimização do 'main.py', transformando o match case em uma função no arquivo 'functions.py'

--- main.py
+++ main.py
@@ -1,49 +1,10 @@
+# Import das funções de functions.py
 from functions import *
-import sys
-import time
 
+# Criação da variável que encerra o While
 encerra_while = False
 
-while encerra_while == False:
+# While para escolher alguma opção válida do menu
+while not encerra_while:
     opcao = entradaOpcao()
-
-    match opcao:
-        case '1':
-            try:
-                adicionarDados()
-                print('Dado adicionado com sucesso!')
-                encerra_while = True
-            except:
-                print('Ocorreu um erro ao adicionar o dado!')
-        case '2':
-            try:
-                lerDados()
-                print('Dados lidos com sucesso!')
-                encerra_while = True
-            except:
-                print('Ocorreu um erro ao ler os dados!')
-        case '3':
-            try:
-                atualizarDados()
-                print('Dados atualizados com sucesso!')
-                encerra_while = True
-            except:
-                print('Ocorreu um erro ao atualizar os dados!')
-        case '4':
-            try:
-                excluirDados()
-                print('Dados excluídos com sucesso!')
-                encerra_while = True
-            except:
-                print('Ocorreu um erro ao excluir os dados!')
-        case '5':
-            print('Saindo do programa...')
-            sys.exit()
-        case _:
-            print('Opção inválida!')
-            print('.')
-            time.sleep(0.4)
-            print('.')
-            time.sleep(0.4)
-            print('.')
-            time.sleep(0.4)
+    encerra_while = validacao(opcao)
